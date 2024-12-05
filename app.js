78cb81985247a19914041fe34aec4928fc36047e
Rafay-Gigani <rafaykarim250@gmail.com>
2024-12-05 21:55:04 +0500

    javascript5-12-2024

---
+++ app.js
@@ -0,0 +1,35 @@
+
+var topMovies = []
+
+topMovies.push('Avergers: Age of Ultron')
+topMovies.push('Spectre')
+topMovies.push('jurassic World')
+topMovies.push('Inside Out')
+
+
+document.write("<h1>Top Movies Of 2015</h1>")
+for (var i = 0; i <=3;  i++) {
+    document.write("<br>" + topMovies[i]);
+}
+document.write("<p>Length of the array: " + topMovies.length + "</p>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
